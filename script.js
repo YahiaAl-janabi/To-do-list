78e467da10e0697d6e850947dab375f31ea008af
Yahia Al-janabi <yahiaaljanabi@gmail.com>
2021-01-16 16:19:03 +0000

    added js to make the list hidden until something is added to the list

--- script.js
+++ script.js
@@ -10,13 +10,33 @@ function addListItem() {
   //when there is no value in the input
   if (inputValue === "") {
     alert("please write something");
-  } //when there is a value in the input the li variable that represents the html li element with the input value is appended to the ul element
+  }
+  //when there is a value in the input the li variable that represents the html li element with the input value is appended to the ul element
   else {
     document.querySelector("ul").appendChild(li);
   }
 }
 
+function listBorderHide() {
+  var ul = document.querySelector("ul");
+  if ((ul.innerHTML = " ")) {
+    ul.style.display = "none";
+  }
+}
+
+function listBorderShow() {
+  var ul = document.querySelector("ul");
+  if (ul.innerHTML != " ") {
+    ul.style.display = "block";
+  }
+}
+
+document.querySelector("body").onload = function () {
+  listBorderHide();
+};
+
 //This is the anonymous function for listening for the event handler
 document.getElementById("todo-submit").onclick = function () {
   addListItem();
+  listBorderShow();
 };
