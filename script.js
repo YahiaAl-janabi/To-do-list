cba85f94e308769fd8ef5fb2b48c2d7ace251520
Yahia Al-janabi <yahiaaljanabi@gmail.com>
2021-01-16 14:32:44 +0000

    added comments

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-function AddListItem() {
+function addListItem() {
+  //creation of the li variable that represents the html li element.
   var li = document.createElement("li");
+  //creation of the inputValue variable that represents the value of the input.
   var inputValue = document.getElementById("todo-input").value;
+  //creation of the html input value.
   var t = document.createTextNode(inputValue);
+  //appending the html input value the li variable that represents the html li element.
   li.appendChild(t);
+  //when there is no value in the input
   if (inputValue === "") {
-    alert("you must write something!");
-  } else {
+    alert("please write something");
+  } //when there is a value in the input the li variable that represents the html li element with the input value is appended to the ul element
+  else {
     document.querySelector("ul").appendChild(li);
   }
-
-  console.log(li.innerHTML);
 }
 
+//This is the anonymous function for listening for the event handler
 document.getElementById("todo-submit").onclick = function () {
-  AddListItem();
+  addListItem();
 };
